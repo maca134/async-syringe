948807c5324ef1e32a143d750e58d6a001ee6784
maca134 <maca134@googlemail.com>
2019-12-09 23:55:54 +0000

    patch

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 if (typeof Reflect === 'undefined' || !Reflect.getMetadata) {
-	throw `tsyringe requires a reflect polyfill. Please add 'import "reflect-metadata"' to the top of your entry point.`;
+	throw `@maca134/async-syringe requires a reflect polyfill. Please add 'import "reflect-metadata"' to the top of your entry point.`;
 }
 export const INJECTION_TOKEN_METADATA_KEY = 'ioc-tokens';
 export const REG_OPTS_METADATA_KEY = 'ioc-opts';
